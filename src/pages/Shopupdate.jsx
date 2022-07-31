4b4c259a243be46ec91de54c114d35348a2aaf0f
Purge <65049931+Purge11@users.noreply.github.com>
2022-07-31 17:03:04 +0300

    Update Shopupdate.jsx

--- src/pages/Shopupdate.jsx
+++ src/pages/Shopupdate.jsx
@@ -7,7 +7,7 @@ export default function Shopupdate() {
                 <div className="row series-image-list ">
                     Hello NosVoid players,
                     <br />
-                    New Fortune Wheel update is alive. [24.07.2022]
+                    New Fortune Wheel update is alive. [31.07.2022]
                     <br />
                     <br />
                     <h3>What can i get from the Fortune Wheel?</h3>
@@ -17,13 +17,14 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Into the Void</th>
-                                    <th scope="col"><img src="asset/shop/kov.webp" alt="" /></th>
+                                    <th scope="col">Mad March Hare Partner Card (S Buff)</th>
+                                    <th scope="col"><img src="asset/EasterEvent/march.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - All attacks are increased by 5%.<br />
-                                    - All defences are increased by 5%.<br />
-                                    - Your specialist's overall skill points are increased by 4.
+                                    - There is 5% chance that picked up gold will be doubled.<br />
+                                    - Increases Gold earned by 20%.<br />
+                                    - Damage to all monsters is increased by 20%.<br />
+                                    - Movement speed is increased by 1.
                                 </d>
                             </thead>
                         </table>
@@ -32,21 +33,14 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Blue Amora's Blessing</th>
+                                    <th scope="col">Caligor's Golden Horn</th>
+                                    <th scope="col"><img src="asset/shop/caligorhelmet.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - All defences are increased by 5%.<br />
-                                    - All attacks are increased by 5%.<br />
-                                    - Movement speed is increased by 1.<br />
-                                    - There is a 3% chance of causing Amora Heal.
+                                    - Increases PvP attack power by 10%.<br />
+                                    - Reduces damage received in PvP by 10%.<br />
+                                    - Up to level 4 there is a 15% chance of never getting a bad effect.
                                 </d>
-                                <br /><br />
-                                <d className="text-success">
-                                    Amora Heal<i>(3 seconds)</i>:
-                                    <br />
-                                    - MP is increased by 5% of damage given<br />
-                                    - HP is increased by 3% of damage given.
-                                    </d>
                             </thead>
                         </table>
                     </div>
@@ -55,15 +49,14 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Aegir Partner Card (S Buff)</th>
-                                    <th scope="col"><img src="asset/shop/aegir.webp" alt="" /></th>
+                                    <th scope="col">Fluffy Pet</th>
+                                    <th scope="col"><img src="asset/EasterEvent/fluffy.png" alt="" /></th>
 
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - Meele attacks are increased by 12%.<br />
-                                    - All defence powers are increased by 150.<br />
-                                    - Hit rate of all attacks is increased by 80.<br />
-                                    - Increases damage in Christmas Raid by 5%.
+                                    - All attacks are increased by 5%.<br />
+                                    - All defences are increased by 5%.<br />
+                                    - Reduces damage received in PvP by 10%.
 
                                 </d>
                             </thead>
@@ -73,31 +66,39 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Easter Bunny Party Set</th>
+                                    <th scope="col">Knight Costume Set</th>
                                 </tr>
                                 <br />
                                 Hat:
                                 <br />
                                 <d className="item-effect-shell">
-                                    - When raid is finished, there's a 2% chance that box drops second time.<br />
-                                    - Increased damage by 10% against raid bosses.
+                                    - All attacks are increased by 6%.<br />
+                                    - Hit rate of all attacks is increased by 100.<br />
+                                    - Chance of inflicting critical hits is increased by 3%.
                                 </d>
                                 <br />
                                 <br />
                                 Costume:
                                 <br />
                                 <d className="item-effect-shell">
-                                    - All defences are increased by 4%.<br />
-                                    - Maximum HP is increased by 2000.
+                                    - All defences are increased by 8%.<br />
+                                    - Movement Speed is increased by 1.<br />
+                                    - Up to level 4 there is a 8% chance of never getting a bad effect.
                                 </d>
                                 <br />
                                 <br />
                                 Costume Wings:
                                 <br />
                                 <d className="item-effect-shell">
-                                    - Increases fame received by 5%.<br />
-                                    - Maximum HP is increased by 2000.<br />
-                                    - Movement speed is increased by 1.
+                                    - All attacks are increased by 85.<br />
+                                    - Your specialist's overall skill points are increased by 3.<br />
+                                    - There is a 4% chance of causing Last Hope.
+                                    </d>
+                                    <br /><br />
+                                <d className="text-success">
+                                    Last Hope<i>(3 seconds)</i>:
+                                    <br />
+                                    - Up to level 5 there is a 25% chance of never getting a bad effect.<br />
                                     </d>
                             </thead>
                         </table>
@@ -106,14 +107,13 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Barni Partner Card (S Buff)</th>
-                                    <th scope="col"><img src="asset/shop/barni.webp" alt="" /></th>
+                                    <th scope="col">Shadow King's Helmet</th>
+                                    <th scope="col"><img src="asset/shop/shadowhelmet.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - Ranged attacks are increased by 12%.
-                                    - Dodge is increased by 80.<br />
-                                    - Increases damage from critical hits by 15%.<br />
-                                    - Increases damage in Christmas Raid by 5%.
+                                    - Damage in Raids is increased by 8%.<br />
+                                    - All elemental resistance is increased by 7.<br />
+                                    - Chance of inflicting critical hits is increased by 3%.
                                 </d>
                             </thead>
                         </table>
@@ -122,14 +122,13 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Lucy Partner Card (S Buff)</th>
-                                    <th scope="col"><img src="asset/EasterEvent/lucy.png" alt="" /></th>
+                                    <th scope="col">Shadow King's Mask</th>
+                                    <th scope="col"><img src="asset/shop/shadowmask.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - Damage in Raids is increased by 15%.<br />
-                                    - Reduces the enemy's elemental resistances by 10.<br />
-                                    - The equipped fairy's element increases by 10.<br />
-                                    - Movement speed is increased by 1.
+                                    - Damage in Raids is increased by 10%.<br />
+                                    - Increases Gold earned by 7%.<br />
+                                    - Damage to all monsters is increased by 7%.
                                 </d>
                             </thead>
                         </table>
@@ -138,14 +137,11 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Koala Pet</th>
+                                    <th scope="col">Betting Costume</th>
+                                    <th scope="col"><img src="asset/shop/beting.webp" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - When raid is finished, there's a 5% chance that box drops second time.<br />
-                                    - When you finish Act4 and Caligor Raid you have 10% chance to get additional box.<br />
-                                    - Increases fame received by 15%.<br />
-                                    - Increased gold earned by 15%.<br />
-                                    - Damage in Raids is increased by 5%.
+                                    - There's 10% more chance of getting rarity 7-8 while betting
                                 </d>
                             </thead>
                         </table>
